d44af682d973cb1a206c28c31a096666f12b1254
Uros Dasic <uros1992dasic@gmail.com>
2023-04-14 22:23:45 +0200

    add prisma orm

---
+++ src/user/models/user.entity.ts
@@ -0,0 +1,25 @@
+import { User } from '@prisma/client';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class UserEntity implements User {
+    @ApiProperty()
+    id: number;
+
+    @ApiProperty()
+    createdAt: Date;
+
+    @ApiProperty()
+    updatedAt: Date;
+
+    @ApiProperty()
+    firstName: string;
+
+    @ApiProperty()
+    lastName: string;
+
+    @ApiProperty()
+    email: string;
+
+    @ApiProperty()
+    password: string;
+}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
-  controllers: [UserController]
+  controllers: [UserController],
+  providers: [PrismaService]
 })
-export class UserModule {}
+export class UserModule { }
