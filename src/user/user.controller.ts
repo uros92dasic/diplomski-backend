2a934a75a02cf9ec2318493f3bdcab7ae63ebd44
Uros Dasic <uros1992dasic@gmail.com>
2023-04-14 22:30:31 +0200

    user service

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('users')
 export class UserController {
+    constructor(private readonly userService: UserService) { }
 
     @Get()
-    all() {
-        return ['users'];
+    getAll() {
+        return this.userService.findAll();
     }
 }
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { UserService } from './user.service';
 
 @Module({
   controllers: [UserController],
-  providers: [PrismaService]
+  providers: [PrismaService, UserService]
 })
 export class UserModule { }
