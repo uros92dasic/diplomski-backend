793c8f6802eb45de49671bbdcf67f085a5d4f4a7
Uros Dasic <uros1992dasic@gmail.com>
2023-05-02 13:14:09 +0200

    fix order create and delete functionality

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -19,38 +19,18 @@ async function main() {
             firstName: "admin",
             lastName: "admin",
             email: "admin@admin.com",
-            password: "1234",
+            password: "$2b$12$ihSmkgjDOe09o5d/ZrvOTOEGbL49NhogAqSdQfGtiUowaS87SCDNi",
             roleId: 1
 
         }
     })
-    const editor = await prisma.user.create({
+    const user = await prisma.user.create({
         data: {
-            firstName: "editor",
-            lastName: "editor",
-            email: "editor@editor.com",
-            password: "1234",
+            firstName: "user",
+            lastName: "user",
+            email: "user@user.com",
+            password: "$2b$12$R6nQD0samMfhdnZ1mCtZxOTZOGL/TeAOJvXgT7mgNvA3MgDaBahbi",
             roleId: 2
-
-        }
-    })
-    const viewer = await prisma.user.create({
-        data: {
-            firstName: "viewer",
-            lastName: "viewer",
-            email: "viewer@viewer.com",
-            password: "1234",
-            roleId: 3
-
-        }
-    })
-    const guest = await prisma.user.create({
-        data: {
-            firstName: "guest",
-            lastName: "guest",
-            email: "guest@guest.com",
-            password: "1234",
-            roleId: 4
 
         }
     })
@@ -58,91 +38,181 @@ async function main() {
     //products
     const product1 = await prisma.product.create({
         data: {
-            title: "test1",
+            title: "test1admin",
             description: "test1",
             image: "test1",
-            price: 110,
+            price: 510,
             userId: 1
         }
     });
     const product2 = await prisma.product.create({
         data: {
-            title: "test2",
+            title: "test2admin",
             description: "test2",
             image: "test2",
-            price: 120,
+            price: 520,
             userId: 1
         }
     });
     const product3 = await prisma.product.create({
         data: {
-            title: "test3",
+            title: "test3admin",
             description: "test3",
             image: "test3",
-            price: 130,
+            price: 530,
             userId: 1
         }
     });
     const product4 = await prisma.product.create({
         data: {
-            title: "test4",
+            title: "test4admin",
             description: "test4",
             image: "test4",
-            price: 140,
+            price: 540,
             userId: 1
         }
     });
     const product5 = await prisma.product.create({
         data: {
-            title: "test5",
+            title: "test5admin",
             description: "test5",
             image: "test5",
-            price: 150,
+            price: 550,
             userId: 1
         }
     });
     const product6 = await prisma.product.create({
         data: {
-            title: "test6",
+            title: "test6user",
             description: "test6",
             image: "test6",
-            price: 160,
-            userId: 1
+            price: 260,
+            userId: 2
         }
     });
     const product7 = await prisma.product.create({
         data: {
-            title: "test7",
+            title: "test7user",
             description: "test7",
             image: "test7",
-            price: 170,
+            price: 270,
             userId: 2
         }
     });
     const product8 = await prisma.product.create({
         data: {
-            title: "test8",
+            title: "test8user",
             description: "test8",
             image: "test8",
-            price: 180,
+            price: 280,
             userId: 2
         }
     });
     const product9 = await prisma.product.create({
         data: {
-            title: "test9",
+            title: "test9user",
             description: "test9",
             image: "test9",
-            price: 190,
+            price: 290,
             userId: 2
         }
     });
     const product10 = await prisma.product.create({
         data: {
-            title: "test10",
+            title: "test10user",
             description: "test10",
             image: "test10",
-            price: 200,
+            price: 300,
+            userId: 2
+        }
+    });
+    const product11 = await prisma.product.create({
+        data: {
+            title: "test11user",
+            description: "test11",
+            image: "test11",
+            price: 310,
+            userId: 2
+        }
+    });
+    const product12 = await prisma.product.create({
+        data: {
+            title: "test12user",
+            description: "test12",
+            image: "test12",
+            price: 320,
+            userId: 2
+        }
+    });
+    const product13 = await prisma.product.create({
+        data: {
+            title: "test13user",
+            description: "test13",
+            image: "test13",
+            price: 330,
+            userId: 2
+        }
+    });
+    const product14 = await prisma.product.create({
+        data: {
+            title: "test14user",
+            description: "test14",
+            image: "test14",
+            price: 340,
+            userId: 2
+        }
+    });
+    const product15 = await prisma.product.create({
+        data: {
+            title: "test15user",
+            description: "test15",
+            image: "test15",
+            price: 350,
+            userId: 2
+        }
+    });
+    const product16 = await prisma.product.create({
+        data: {
+            title: "test16user",
+            description: "test16",
+            image: "test16",
+            price: 360,
+            userId: 2
+        }
+    });
+    const product17 = await prisma.product.create({
+        data: {
+            title: "test17user",
+            description: "test17",
+            image: "test17",
+            price: 370,
+            userId: 2
+        }
+    });
+    const product18 = await prisma.product.create({
+        data: {
+            title: "test18user",
+            description: "test18",
+            image: "test18",
+            price: 380,
+            userId: 2
+        }
+    });
+    const product19 = await prisma.product.create({
+        data: {
+            title: "test19user",
+            description: "test19",
+            image: "test19",
+            price: 390,
+            userId: 2
+        }
+    });
+    const product20 = await prisma.product.create({
+        data: {
+            title: "test20user",
+            description: "test20",
+            image: "test20",
+            price: 400,
             userId: 2
         }
     });
--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateOrderDto } from './models/create-order.dto';
-import { UpdateOrderDto } from './models/update-order.dto';
-import { UpdateOrderItemDto } from './models/update-order-item.dto';
 
 @Injectable()
 export class OrderService {
@@ -20,43 +18,20 @@ export class OrderService {
                     include: {
                         product: {
                             include: {
-                                user: true
+                                user: {
+                                    include: {
+                                        role: true
+                                    }
+                                }
                             }
                         }
                     }
-                }
+                },
+                user: true
             }
         });
 
         const total = await this.prisma.order.count();
-
-        return {
-            data: roles,
-            meta: {
-                total,
-                page,
-                lastPage: Math.ceil(total / take)
-            }
-        }
-    }
-
-    async paginateOrderItems(page = 1) {
-        const take = 15;
-        const skip = (page - 1) * take;
-
-        const roles = await this.prisma.orderItem.findMany({
-            take,
-            skip,
-            include: {
-                product: {
-                    include: {
-                        user: true
-                    }
-                }
-            }
-        });
-
-        const total = await this.prisma.orderItem.count();
 
         return {
             data: roles,
@@ -84,7 +59,7 @@ export class OrderService {
         for (const productData of createOrderDto.products) {
             // Find the product to get its price
             const product = await this.prisma.product.findUnique({
-                where: { id: productData.productId },
+                where: { id: +(productData.productId) },
             });
 
             if (!product) {
@@ -98,8 +73,8 @@ export class OrderService {
             await this.prisma.orderItem.create({
                 data: {
                     orderId: order.id,
-                    productId: productData.productId,
-                    quantity: productData.quantity,
+                    productId: +(productData.productId),
+                    quantity: +(productData.quantity),
                 },
             });
         }
@@ -113,54 +88,22 @@ export class OrderService {
         return order;
     }
 
-    async updateOrder(orderId: number, updateOrderDto: UpdateOrderDto) {
-        return await this.prisma.order.update({
+    async deleteOrder(orderId: number) {
+        // Delete related order items first
+        await this.prisma.orderItem.deleteMany({
+            where: { orderId },
+        });
+
+        // Delete the order
+        await this.prisma.order.delete({
             where: { id: orderId },
-            data: {
-                total: updateOrderDto.total,
-            },
         });
     }
 
-    async updateOrderItem(orderItemId: number, updateOrderItemDto: UpdateOrderItemDto) {
-        return await this.prisma.orderItem.update({
-            where: { id: orderItemId },
-            data: {
-                quantity: updateOrderItemDto.quantity,
-            },
-        });
-    }
-
-    async deleteOrderItem(orderItemId: number) {
-        // First, find the order item to get the order it belongs to
-        const orderItem = await this.prisma.orderItem.findUnique({
-            where: { id: orderItemId },
-        });
-
-        if (!orderItem) {
-            throw new Error('Order item not found');
-        }
-
-        // Delete the order item
-        await this.prisma.orderItem.delete({
-            where: { id: orderItemId },
-        });
-
-        // Recalculate the order total
-        const orderItems = await this.prisma.orderItem.findMany({
-            where: { orderId: orderItem.orderId },
-            include: { product: true }
-        });
-
-        const total = orderItems.reduce(
-            (sum, item) => sum + item.quantity * item.product.price,
-            0
-        );
-
-        // Update the order total
-        await this.prisma.order.update({
-            where: { id: orderItem.orderId },
-            data: { total },
+    async getOrderById(orderId: number) {
+        return await this.prisma.order.findUnique({
+            where: { id: orderId },
+            include: { orderItems: { include: { product: true } } },
         });
     }
 
